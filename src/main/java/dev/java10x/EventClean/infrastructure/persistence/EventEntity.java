1763be336de57c4f1ba82d8b4cc4cbe1705d1de4
Matheus Yuri Silva <matheusyurirs@gmail.com>
2025-05-07 16:17:49 -0300

    feat: add V1__create_table_events.sql

---
+++ src/main/java/dev/java10x/EventClean/infrastructure/persistence/EventEntity.java
@@ -0,0 +1,4 @@
+package dev.java10x.EventClean.infrastructure.persistence;
+
+public class EventEntity {
+}
---
+++ src/main/resources/db/migration/V1__create_table_evento.sql
@@ -0,0 +1,13 @@
+-- V1_CREATE_TABLE_EVENTS.SQL
+CREATE TABLE Eventos(
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
+    identificador VARCHAR(255) NOT NULL UNIQUE,
+    data_inicio_evento TIMESTAMP NOT NULL,
+    data_fim_evento TIMESTAMP NOT NULL,
+    local_do_evento VARCHAR(255) NOT NULL,
+    capacidade INTEGER NOT NULL,
+    organizador VARCHAR(255) NOT NULL,
+    tipo_evento VARCHAR(100)
+);
